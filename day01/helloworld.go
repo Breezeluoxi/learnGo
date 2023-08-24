f97e401f13d0bbcbd8ee4ae6f725a88bd33e3fc6
镜名 <pujingming@ichangtou.com>
2023-08-24 13:25:51 +0800

    代理程序提交

--- day01/helloworld.go
+++ day01/helloworld.go
@@ -1,20 +1,51 @@
-package test
+package main
 
 import (
-	"fmt"
-	"os/user"
-	"runtime"
+	"flag"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/elazarl/goproxy"
 )
 
 func main() {
-	var user, error = user.Current()
-	if error == nil {
-		fmt.Println("hello,", user.Username)
-	}
-	// 计算机主板的型号
-	fmt.Println(runtime.GOARCH)
-	// 操作系统的类型
-	fmt.Println(runtime.GOOS)
-	// 定义数组
+	// 定义命令行参数
+	port := flag.String("port", "1314", "Proxy server port")
+	// path := flag.String("path", "./", "work dir log out path")
 
+	// 解析命令行参数
+	flag.Parse()
+
+	// 创建一个反向代理
+	proxy := goproxy.NewProxyHttpServer()
+
+	// 创建日志记录器
+	logFile := &lumberjack.Logger{
+		Filename: "proxy.log", // 日志文件名
+
+		MaxSize:    100, // 单个日志文件最大尺寸，单位 MB
+		MaxBackups: 3,   // 最多保留的旧日志文件数
+		MaxAge:     7,   // 旧日志文件保留的天数
+	}
+
+	// 将日志输出重定向到日志记录器
+	log.SetOutput(logFile)
+
+	// 添加日志打印功能
+	proxy.Verbose = true
+
+	// 修改代理处理函数，记录客户端信息
+	proxy.OnRequest().DoFunc(func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		// 打印请求信息
+		log.Printf("Incoming Request from: %s - %s %s", ctx.Req.RemoteAddr, r.Method, r.URL.Path)
+
+		// 返回 nil 表示不修改请求和响应
+		return r, nil
+	})
+
+	// 启动代理服务器
+	addr := ":" + strings.TrimLeft(*port, ":") // 确保端口前面有冒号
+	log.Printf("Proxy server listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, proxy))
 }
